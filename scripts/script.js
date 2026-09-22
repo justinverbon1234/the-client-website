b4d0cad54cf0c153760cd0d6806ecd7acc412e74
Justin <>
2026-09-22 20:15:22 +0200

    added js, added a hero section and styled it

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-// JS
+const menuToggle = document.querySelector('.menu-toggle');
+const mainNav = document.querySelector('.main-nav');
+
+menuToggle.addEventListener('click', () => {
+  const isOpen = mainNav.classList.toggle('is-open');
+
+  menuToggle.setAttribute('aria-expanded', isOpen);
+});
